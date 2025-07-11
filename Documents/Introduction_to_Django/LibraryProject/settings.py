62262f8edacb6297f7dff62e5fee10f0bf4e6615
nash052842 <mwanjonahashon@gmail.com>
2025-07-11 19:23:47 +0300

    first commit

--- Documents/Introduction_to_Django/LibraryProject/settings.py
+++ Documents/Introduction_to_Django/LibraryProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
+
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'world',
+        'USER': 'root',
+        'PASSWORD': 'shem',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
